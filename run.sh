0d21c8036d94c5cb45e62a03a6eb31c556e24b25
lema <kosgorbunov@gmail.com>
2022-08-06 13:51:13 +0300

    starting consul

--- run.sh
+++ run.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
-echo Task implementation purposed
+echo CTask implementation purposed
 echo ----------------------------
 
 echo Click 1
 echo ------------
 echo 1st click for initializing your environment
 echo -------------------------------------------
-echo Clicking...
+echo Clicking 1...
+
+echo Starting consul server
+docker pull consul
+docker images -f 'reference=consul'
+docker run \
+    -d \
+    -p 8500:8500 \
+    -p 8600:8600/udp \
+    --name=consul-server \
+    consul agent -server -ui -node=server-1 -bootstrap-expect=1 -client=0.0.0.0
+
