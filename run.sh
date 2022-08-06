da162990e4efd15feb1b60ffeaa1ad3bd0b58369
lema <kosgorbunov@gmail.com>
2022-08-06 14:08:49 +0300

    consul stuff: 2

--- run.sh
+++ run.sh
@@ -1,30 +1,48 @@
 #!/usr/bin/env bash
 
-echo CTask implementation purposed
-echo ----------------------------
+init() {
+  export consul_server=csrv
+}
 
-echo Click 1
-echo ------------
-echo 1st click for initializing your environment
-echo -------------------------------------------
-echo Clicking 1...
-
-#####################
-consul-server=csrv
-#####################
-echo Starting consul server
-docker pull consul
-docker images -f 'reference=consul'
-docker run \
+csrv_start() {
+  echo Starting consul server
+  docker pull consul
+  docker images -f 'reference=consul'
+  docker run \
     -d \
     -p 8500:8500 \
     -p 8600:8600/udp \
-    --name=$consul-server \
+    --name=$consul_server \
     consul agent -server -ui -node=server-1 -bootstrap-expect=1 -client=0.0.0.0
+}
 
+click1() {
+  echo CTask implementation purposed
+  echo ----------------------------
+  echo Click 1
+  echo ------------
+  echo 1st click for initializing your environment
+  echo -------------------------------------------
+  echo Clicking 1...
 
-docker ps
-sleep 10
-echo Stopping consul server
-docker stop $consul-server
+  csrv_start
+}
 
+justwaiting() {
+  docker ps
+  echo "waiting ${1} seconds"
+  sleep "${1}"
+
+}
+cleanup() {
+  echo Cleanup now...
+  echo ----------------
+  echo Cleaning...
+  echo Stopping consul server
+  docker stop $consul_server
+}
+
+init
+click1
+justwaiting 10
+cleanup
