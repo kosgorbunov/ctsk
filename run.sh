cfa4ca1fe7bd4200090057c8d4096cf5c3f02d7c
lema <kosgorbunov@gmail.com>
2022-08-06 13:44:13 +0300

    2

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+
+echo Task implementation purposed
+echo ----------------------------
+
+echo Click 1
+echo ------------
+echo 1st click for initializing your environment
+echo -------------------------------------------
+echo Clicking...
