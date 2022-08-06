85667fbcbfef5e25a0a5e0d2349619baa3146c8c
lema <kosgorbunov@gmail.com>
2022-08-06 19:13:31 +0300

    jenkins: 30

--- run.sh
+++ run.sh
@@ -76,8 +76,8 @@ jenkinsStart() {
   echo Starting Jenkins
   echo ">>>>>>>>>>>>>>>>"
   mkdir -vp $jenkins_home | tee -a $logfile &>/dev/null
-#  export JAVA_OPTS=-Djenkins.install.runSetupWizard=false
-#  export JENKINS_OPTS=--argumentsRealm.roles.user=admin --argumentsRealm.passwd.admin=admin --argumentsRealm.roles.admin=admin
+  #  export JAVA_OPTS=-Djenkins.install.runSetupWizard=false
+  #  export JENKINS_OPTS=--argumentsRealm.roles.user=admin --argumentsRealm.passwd.admin=admin --argumentsRealm.roles.admin=admin
   docker run \
     -d \
     -p 8080:8080 -p 50000:50000 \
@@ -87,17 +87,15 @@ jenkinsStart() {
     -v $jenkins_home:/var/jenkins_home \
     jenkins/jenkins:lts-jdk11 | tee -a $logfile &>/dev/null
 
+  #  while ! nc -z localhost 8080; do
+  #    sleep 0.5
+  #    echo -n .
+  #  done
+
   echo "Waiting jenkins to launch on 8080..."
-  while ! nc -z localhost 8080; do
-    sleep 0.5
-    echo -n .
-  done
+  wget --tries=30 http://localhost:8080/ && echo "Jenkins up and running" && echo ------------------------ && echo "URL is http://localhost:8080/"
+  #  curl -v http://localhost:8080/
 
-  echo "Jenkins up and running"
-  echo ------------------------
-  echo "URL is http://localhost:8080/"
-
-#  curl -v http://localhost:8080/
   echo
 }
 
@@ -133,24 +131,23 @@ jenkinsActivate() {
   #  echo "Step2: paste there next passphras: $(cat  /tmp/jenkins_home/secrets/initialAdminPassword)"
   #  echo "Step3: push the button 'Install suggested plugins'"
 
-#  echo "Waiting initialAdminPassword to appear..."
-#
-#  while [ ! -f /tmp/jenkins_home/secrets/initialAdminPassword ]; do
-#    sleep 0.5
-#    echo -n .
-#  done
-#  echo
+  #  echo "Waiting initialAdminPassword to appear..."
+  #
+  #  while [ ! -f /tmp/jenkins_home/secrets/initialAdminPassword ]; do
+  #    sleep 0.5
+  #    echo -n .
+  #  done
+  #  echo
 
-#  curl -v -d "security-token=$(cat /tmp/jenkins_home/secrets/initialAdminPassword)" -X POST http://localhost:8080/login?from=%2F
+  #  curl -v -d "security-token=$(cat /tmp/jenkins_home/secrets/initialAdminPassword)" -X POST http://localhost:8080/login?from=%2F
 
-#  JSESSIONID=$(curl -X GET -sS -H "Authorization: Bearer $SA_TOKEN" -D - "http://localhost:8080" -o /dev/null | grep JSESSIONID | sed "s/^Set-Cookie: \(.*\); Path=\(.*\)$/\1/")
+  #  JSESSIONID=$(curl -X GET -sS -H "Authorization: Bearer $SA_TOKEN" -D - "http://localhost:8080" -o /dev/null | grep JSESSIONID | sed "s/^Set-Cookie: \(.*\); Path=\(.*\)$/\1/")
 
-#  echo "Step1: enter $(cat /tmp/jenkins_home/secrets/initialAdminPassword) in http://localhost:8080/"
-#  echo "Step2: push the button 'Install suggested plugins'"
-#  echo "Step3: user creation: skip and continue as admin"
-#  echo "Step4: keep Jenkins URL as default"
-#  echo "Step5: Save and Finish"
-
+  #  echo "Step1: enter $(cat /tmp/jenkins_home/secrets/initialAdminPassword) in http://localhost:8080/"
+  #  echo "Step2: push the button 'Install suggested plugins'"
+  #  echo "Step3: user creation: skip and continue as admin"
+  #  echo "Step4: keep Jenkins URL as default"
+  #  echo "Step5: Save and Finish"
 
   echo
 }
