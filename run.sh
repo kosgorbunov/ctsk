a4ff92a4071a42139d248f048273ad0d2e5a6cf6
lema <kosgorbunov@gmail.com>
2022-08-06 14:40:56 +0300

    consul stuff: 12

--- run.sh
+++ run.sh
@@ -8,30 +8,31 @@ cleanup() {
   test $? -eq 0 || echo "no any"
   echo -n "Removing container: "
   docker rm $consul_server 2>/dev/null
-  test $? -eq 0 || echo -n "no any"
+  test $? -eq 0 || echo "no any"
   docker ps -a | grep $consul_server
+  touch $logfile
   echo --------------
   echo
-
 }
 
 init() {
   echo CTask implementation purposed
   echo -----------------------------
   export consul_server=csrv
+  export logfile=all.log
   cleanup
 }
 
 csrv_start() {
   echo Starting consul server
-  docker pull consul
+  docker pull consul 2>&1 | tee -a $logfile
   docker images -f 'reference=consul'
   docker run \
     -d \
     -p 8500:8500 \
     -p 8600:8600/udp \
     --name=$consul_server \
-    consul agent -server -ui -node=server-1 -bootstrap-expect=1 -client=0.0.0.0 || tee -a all.log
+    consul agent -server -ui -node=server-1 -bootstrap-expect=1 -client=0.0.0.0 2>&1 | tee -a $logfile
 }
 
 click1() {
